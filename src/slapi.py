b3e3be26c3a7edbae3f315e493a5ab30f565bfac
Ashley Elizabeth Hoover <hoover28@hagrid7.llnl.gov>
2026-09-08 11:09:00 -0700

    added response for successful move abort

--- src/slapi.py
+++ src/slapi.py
@@ -1167,6 +1167,9 @@ class SpectraLogicAPI:
             api_response = api_instance.abort_move(task_id)
             print(api_response)
             abort_task_id = api_response.task_id
+
+            if abort_task_id == task_id:
+                print("Move abort was successfull")
             
     def robotservice(self, robot=None, action=None, wait=True):
 
